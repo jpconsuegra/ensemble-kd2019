5ecf8b85cf55b2fbcbd36cc3ae7b63eff97d16bf
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2020-03-27 13:05:03 +0000

    Implement performance_per_agreement in extract

--- scripts/extract.py
+++ scripts/extract.py
@@ -8,7 +8,7 @@ from scripts.ensemble.utils import keep_non_annotated_sentences
 from scripts.utils import Collection, CollectionV1Handler, CollectionV2Handler
 
 
-def sort_sentences(collection: EnsembledCollection):
+def sort_sentences(collection: EnsembledCollection, *, reverse=False):
     sentences = []
     votes_per_sentence = defaultdict(list)
 
@@ -20,9 +20,9 @@ def sort_sentences(collection: EnsembledCollection):
     for sid, sentence in enumerate(collection.sentences):
         votes = votes_per_sentence[sid]
         score = score_annotations(votes)
-        sentences.append((sentence, score))
+        sentences.append((sid, sentence, score))
 
-    sentences.sort(key=lambda x: x[1])
+    sentences.sort(key=lambda x: x[-1], reverse=reverse)
     return sentences
 
 
@@ -37,20 +37,39 @@ def score_annotations(votes):
 
 
 def normalize(sentences, choir):
-    return [(sentence, score / len(choir.submissions)) for sentence, score in sentences]
+    return [
+        (sid, sentence, score / len(choir.submissions))
+        for sid, sentence, score in sentences
+    ]
 
 
 def plot(sentences, name, **kargs):
-    scores = [score for _, score in sentences]
+    scores = [score for _, _, score in sentences]
     plt.hist(scores, cumulative=True, **kargs)
     plt.hist(scores, **kargs)
     plt.savefig(f"{name}.pdf")
     plt.close()
 
 
+def performance_per_agreement(ensembled: EnsembledCollection, *, normalized=False):
+    gold = Collection()
+    collection = Collection()
+
+    ordered = sort_sentences(ensembled, reverse=True)
+    ordered = normalize(ordered, ensembled.choir) if normalized else ordered
+
+    yield dict(top=0, score=1, f1=1)
+    for i, (sid, _, score) in enumerate(ordered, 1):
+        gold_sent = ensembled.choir.gold.sentences[sid]
+        ensembled_sent = ensembled.collection.sentences[sid]
+        gold.sentences.append(gold_sent)
+        collection.sentences.append(ensembled_sent)
+        yield dict(top=i, score=score, f1=EnsembleChoir.evaluate(collection, gold))
+
+
 if __name__ == "__main__":
-    ps = Path("./data/submissions/all")
-    pg = Path("./data/ehealth2019-testing")
+    ps = Path("./data/ehealth2019/submissions/all")
+    pg = Path("./data/ehealth2019/testing")
 
     choir = EnsembleChoir().load(CollectionV1Handler, ps, pg, best=True)
     choir = keep_non_annotated_sentences(choir)
@@ -63,8 +82,11 @@ if __name__ == "__main__":
     sentences = sort_sentences(collection)
     normalized = normalize(sentences, choir)
 
-    for s, score in normalized:
-        print(f"{s.text}\t{score}")
+    for sid, s, score in normalized:
+        print(f"{sid:4}\t{s.text}\t{score}")
 
     plot(sentences, "hist")
     plot(normalized, "hist-norm")
+
+    for item in performance_per_agreement(collection):
+        print(item)
